683c40483bbedad8828a5451dc85dcf1aa608850
hsichao0221 <hsichao0221@gmail.com>
2026-06-22 07:37:12 +0800

    feat: 加入購物車功能 - src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { CartProvider, useCart } from "./CartContext.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
+import CartPage from "./pages/CartPage.jsx";
+
+function NavBar() {
+  const { totalQty } = useCart();
+  return (
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "12px 20px",
+        borderBottom: "1px solid #eee",
+        position: "sticky",
+        top: 0,
+        background: "#fff",
+        zIndex: 10,
+      }}
+    >
+      <Link to="/" style={{ textDecoration: "none", color: "#222", fontWeight: 700, fontSize: 18, letterSpacing: 1 }}>
+        TATA
+      </Link>
+      <Link to="/cart" style={{ textDecoration: "none", color: "#222", fontSize: 14, position: "relative" }}>
+        🛒 購物車
+        {totalQty > 0 && (
+          <span
+            style={{
+              position: "absolute",
+              top: -8,
+              right: -14,
+              background: "#c0392b",
+              color: "#fff",
+              borderRadius: 10,
+              fontSize: 11,
+              padding: "1px 6px",
+              fontWeight: 700,
+            }}
+          >
+            {totalQty}
+          </span>
+        )}
+      </Link>
+    </nav>
+  );
+}
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products/:sku" element={<ProductPage />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products/:sku" element={<ProductPage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
